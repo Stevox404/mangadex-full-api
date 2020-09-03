77a080c6cc2a88fd12f5e23fa514a57fdcecc739
Benz Stevox <stevox@debian.com>
2020-09-03 13:17:44 +0300

    fix formatMoney handling decimals

--- index.js
+++ index.js
@@ -63,13 +63,13 @@ export function throttle(func, timeFrame) {
 
 export function formatMoney(amount = 0, decimalCount = 2, decimal = ".", thousands = ",") {
     try {
-        amount = ((amount || '') + '').replace(/,/g, '');
+        amount = String(amount || '').replace(/,/g, '');
         decimalCount = Math.abs(decimalCount);
         decimalCount = isNaN(decimalCount) ? 2 : decimalCount;
 
         const negativeSign = amount < 0 ? "-" : "";
 
-        let i = parseFloat(amount = Math.abs(Number(amount) || 0).toFixed(decimalCount)).toString();
+        let i = parseInt(amount = Math.abs(Number(amount) || 0).toFixed(decimalCount)).toString();
         let j = (i.length > 3) ? i.length % 3 : 0;
 
         return negativeSign + (j ? i.substr(0, j) + thousands : '') + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + thousands) + (decimalCount ? decimal + Math.abs(amount - i).toFixed(decimalCount).slice(2) : "");
