2bdd2e5956a8b352fd5dea349d0ca4f836e7688f
Ben Steve <benzstevox@gmail.com>
2022-05-06 22:54:30 +0300

    mfa use es6 export syntax

--- src/Libraries/mfa/src/index.js
+++ src/Libraries/mfa/src/index.js
@@ -6,20 +6,13 @@ const LocalizedString = require('./internal/localizedstring.js');
 const APIRequestError = require('./internal/requesterror.js');
 
 // Export
-const Manga = require('./structure/manga.js');
-exports.Manga = Manga;
-const Author = require('./structure/author.js');
-exports.Author = Author;
-const Chapter = require('./structure/chapter.js');
-exports.Chapter = Chapter;
-const Group = require('./structure/group.js');
-exports.Group = Group;
-const User = require('./structure/user.js');
-exports.User = User;
-const List = require('./structure/list.js');
-exports.List = List;
-const Cover = require('./structure/cover.js');
-exports.Cover = Cover;
+export const Manga = require('./structure/manga.js');
+export const Author = require('./structure/author.js');
+export const Chapter = require('./structure/chapter.js');
+export const Group = require('./structure/group.js');
+export const User = require('./structure/user.js');
+export const List = require('./structure/list.js');
+export const Cover = require('./structure/cover.js');
 
 /**
  * Converts old (pre v5, numeric ids) Mangadex ids to v5 ids.
@@ -29,25 +22,23 @@ exports.Cover = Cover;
  * @param {...Number|Number[]} ids Array of ids to convert
  * @returns {Promise<String[]>}
  */
-async function convertLegacyId(type, ...ids) {
+export async function convertLegacyId(type, ...ids) {
     if (ids.length === 0) throw new Error('Invalid Argument(s)');
     ids = ids.flat();
     let res = await Util.apiRequest('/legacy/mapping', 'POST', { type: type, ids: ids });
     if (!(res.data instanceof Array)) throw new APIRequestError('The API did not respond with an array when it was expected to', APIRequestError.INVALID_RESPONSE);
     return res.data.map(e => e.attributes.newId);
 }
-exports.convertLegacyId = convertLegacyId;
 
 /**
  * Sets the global locaization for LocalizedStrings.
  * Uses 2-letter Mangadex region codes.
  * @param {String} newLocale
  */
-function setGlobalLocale(newLocale) {
+export function setGlobalLocale(newLocale) {
     if (typeof newLocale !== 'string' || newLocale.length !== 2) throw new Error('Invalid Locale Code.');
     LocalizedString.locale = newLocale;
 };
-exports.setGlobalLocale = setGlobalLocale;
 
 const AuthUtil = require('./auth.js');
 
@@ -59,10 +50,9 @@ const AuthUtil = require('./auth.js');
  * @param {String} [cacheLocation] File location (or localStorage key for browsers) to store the persistent token IN PLAIN TEXT
  * @returns {Promise<void>}
  */
-function login(username, password, cacheLocation) {
+export function login(username, password, cacheLocation) {
     return AuthUtil.login(username, password, cacheLocation);
 }
-exports.login = login;
 
 // Register class types to bypass circular references
 const Relationship = require('./internal/relationship.js');
@@ -83,7 +73,6 @@ Relationship.registerType('cover_art', Cover);
  * @param {Relationship<T>[]} relationshipArray
  * @returns {Promise<T[]>}
  */
-function resolveArray(relationshipArray) {
+export function resolveArray(relationshipArray) {
     return Relationship.resolveAll(relationshipArray);
 }
-exports.resolveArray = resolveArray;
